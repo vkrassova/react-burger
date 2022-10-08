269272e6422ec512354e3725ba8ed2d445e49b1a
vkrassova <v.krassova@yandex.ru>
2022-10-08 12:32:25 +0300

    fix tabs

--- src/components/IngredientDetails/IngredientDetails.tsx
+++ src/components/IngredientDetails/IngredientDetails.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import style from './IngredientDetails.module.scss'
+import style from './IngredientDetails.module.scss';
 
 const IngredientDetails = ({ingredient}: any) => {
     return (
--- src/components/Tabs/Tabs.tsx
+++ src/components/Tabs/Tabs.tsx
@@ -2,21 +2,24 @@ import {Tab} from '@ya.praktikum/react-developer-burger-ui-components';
 import React from 'react';
 import styles from '../Tabs/Tabs.module.scss';
 
-const Tabs = () => {
-    const buns = 'buns';
-    const sauces = 'sauces';
-    const main = 'main';
+const INGREDIENT_TYPES = {
+    buns: 'buns',
+    sauces: 'sauces',
+    main: 'main'
+}
 
-    const [current, setCurrent] = React.useState(buns)
+const Tabs = () => {
+    const [current, setCurrent] = React.useState(INGREDIENT_TYPES.buns)
+
     return (
         <div className={styles.tabsWrapper}>
-            <Tab value={buns} active={current === buns} onClick={setCurrent}>
+            <Tab value={INGREDIENT_TYPES.buns} active={current === INGREDIENT_TYPES.buns} onClick={setCurrent}>
                 Булки
             </Tab>
-            <Tab value={sauces} active={current === sauces} onClick={setCurrent}>
+            <Tab value={INGREDIENT_TYPES.sauces} active={current === INGREDIENT_TYPES.sauces} onClick={setCurrent}>
                 Соусы
             </Tab>
-            <Tab value={main} active={current === main} onClick={setCurrent}>
+            <Tab value={INGREDIENT_TYPES.main} active={current === INGREDIENT_TYPES.main} onClick={setCurrent}>
                 Начинки
             </Tab>
         </div>
