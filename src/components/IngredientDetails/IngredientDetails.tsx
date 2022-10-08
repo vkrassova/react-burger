294d0fdeb6cfd9956fa8a162afdf90794a4cf3f1
vkrassova <v.krassova@yandex.ru>
2022-10-08 19:04:36 +0300

    fix ingredients

--- src/components/IngredientDetails/IngredientDetails.tsx
+++ src/components/IngredientDetails/IngredientDetails.tsx
@@ -9,20 +9,20 @@ const IngredientDetails = ({ingredient}: any) => {
             </div>
             <h3 className="text text_type_main-medium mb-8">{ingredient.name}</h3>
             <ul className={style.list}>
-                <li>
-                    <p className="text text_type_main-default text_color_inactive">Калории,ккал</p>
+                <li className={style.item}>
+                    <p className="text text_type_main-default text_color_inactive pb-2">Калории,ккал</p>
                     <span className="text text_type_digits-default text_color_inactive">{ingredient.calories}</span>
                 </li>
-                <li>
-                    <p className="text text_type_main-default text_color_inactive">Калории,ккал</p>
+                <li className={style.item}>
+                    <p className="text text_type_main-default text_color_inactive pb-2">Белки, г</p>
                     <span className="text text_type_digits-default text_color_inactive">{ingredient.proteins}</span>
                 </li>
-                <li>
-                    <p className="text text_type_main-default text_color_inactive">Калории,ккал</p>
+                <li className={style.item}>
+                    <p className="text text_type_main-default text_color_inactive pb-2">Жиры, г</p>
                     <span className="text text_type_digits-default text_color_inactive">{ingredient.fat}</span>
                 </li>
-                <li>
-                    <p className="text text_type_main-default text_color_inactive">Калории,ккал</p>
+                <li className={style.item}>
+                    <p className="text text_type_main-default text_color_inactive pb-2">Углеводы, г</p>
                     <span className="text text_type_digits-default text_color_inactive">{ingredient.carbohydrates}</span>
                 </li>
             </ul>
