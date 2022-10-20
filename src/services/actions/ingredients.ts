2b58bb47f4f595d9f27bf84cdccde9272f440f7f
v.krassova <v.krassova@yandex.ru>
2022-10-20 10:53:59 +0300

    add ingredients actions

---
+++ src/services/actions/ingredients.ts
@@ -0,0 +1,12 @@
+import {API_INGREDIENTS} from '../../const'
+import {Ingredients} from '../../types/data';
+
+export const GET_INGREDIENTS_REQUEST = "GET_INGREDIENTS_REQUEST"
+export const GET_INGREDIENTS_SUCCESS = "GET_INGREDIENTS_SUCCESS"
+export const GET_INGREDIENTS_FAILED = "GET_INGREDIENTS_FAILED"
+
+export const getIngredients = () => {
+     return function (dispatch: Function) {
+
+     }
+}
