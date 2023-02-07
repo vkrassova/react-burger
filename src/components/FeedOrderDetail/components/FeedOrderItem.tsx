34d52c678ce067f326acaeee37279bc150947e19
v.krassova <v.krassova@yandex.ru>
2023-02-07 18:55:55 +0300

    starts order feed detail page

--- src/components/FeedOrderDetail/components/FeedOrderItem.tsx
+++ src/components/FeedOrderDetail/components/FeedOrderItem.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import style from './FeedOrderItem.module.scss'
 import {Ingredients} from '../../../types/data'
 import {OrderImage} from '../../OrderImage/OrderImage'
+import { CurrencyIcon } from '@ya.praktikum/react-developer-burger-ui-components'
 
 type FeedOrderItemProps = {
     element: Ingredients,
@@ -12,6 +13,12 @@ export const FeedOrderItem: React.FC<FeedOrderItemProps> = ({element}) => {
         <li className={`${style.item} mr-6`}>
             <div className={style.imgWrapper}>
                 <OrderImage src={element.image}/>
+            </div>
+            <div className={`text text_type_main-default`}>
+                {element.name}
+            </div>
+            <div className={style.price}>
+                <CurrencyIcon type="primary" />
             </div>
         </li>
     )
