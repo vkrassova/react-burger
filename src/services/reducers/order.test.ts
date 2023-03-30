9344e7db4dd7873a33ae8beed66ea4cf6d9648b9
vkrassova <v.krassova@yandex.ru>
2023-03-30 22:49:58 +0300

    add order test

--- src/services/reducers/order.test.ts
+++ src/services/reducers/order.test.ts
@@ -1,20 +1,54 @@
-import {OrderActions} from "../../types/order"
-
-import {orderActions} from "../constants";
-
-import {orderReducer, initialState} from "./order"
-
-import {orderRequestActions} from "../actions/order";
+import { OrderActions } from "../../types/order"
+import {OrderNumber} from "../../types/responses"
+import { orderActions } from "../constants"
+import { initialState, orderReducer } from "./order"
 
 describe('Проверяет orderReducer', () => {
   it('Проверка начального состояния', () => {
     expect(orderReducer(undefined, {} as OrderActions)).toEqual(initialState)
-  });
+  })
 
-  it('Проверка отправки запроса', () => {
-    const action = {
+  it('Проверка  успешной отправки запроса', () => {
+    const action: OrderActions = {
       type: orderActions.GET_ORDER_REQUEST
     }
-    expect(orderRequestActions()).toEqual(action)
-  });
+    const result = orderReducer(initialState, action)
+
+    expect(result).toEqual({
+      ...initialState,
+      orderRequest: true,
+    })
+  })
+
+  it('Проверка успешного оформления заказа', () => {
+    const order: OrderNumber = {
+      number: 25088
+    }
+
+    const action: OrderActions = {
+      type: orderActions.GET_ORDER_SUCCESS,
+      payload: order
+    }
+
+    const result = orderReducer(initialState, action)
+
+    expect(result).toEqual({
+      ...initialState,
+      order,
+      orderRequest: false,
+      orderFailed: false,
+    })
+  })
+
+  it('Проверка неуспешной отправки запроса', () => {
+    const action: OrderActions = {
+      type: orderActions.GET_ORDER_FAILED
+    }
+    const result = orderReducer(initialState, action)
+
+    expect(result).toEqual({
+      ...initialState,
+      orderFailed: true,
+    })
+  })
 })
